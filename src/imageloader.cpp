326c91a5488426f38eaa70a9c3a3a6fd9837e782
Lionel Bergeret <lbergeret@gmail.com>
2014-11-20 14:47:19 +0900

    Store searched image into jpeg file inside cache folder

--- src/imageloader.cpp
+++ src/imageloader.cpp
@@ -21,6 +21,7 @@
 
 #include <iostream>
 #include <vector>
+#include <stdio.h>
 
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
@@ -28,6 +29,45 @@
 #include <imageloader.h>
 #include <messages.h>
 
+
+#define STORE_SEARCHED_IMAGE
+#define STORE_SEARCHED_IMAGE_TEMPLATE_NAME "cache/%s.jpg"
+#define STORE_SEARCHED_IMAGE_TEMPLATE_SIZE 43
+
+#ifdef STORE_SEARCHED_IMAGE
+#include <md5/md5.h>
+
+// Convert string to MD5 hash
+// From http://answers.opencv.org/question/21035/how-to-convert-cvmat-to-stdstring-and-hash-it/
+char *str2md5(const char *str, int length) {
+    int n;
+    MD5_CTX c;
+    unsigned char digest[16];
+    char digestHexadecimal[33];
+    char *out = (char*)malloc(STORE_SEARCHED_IMAGE_TEMPLATE_SIZE);
+
+    MD5_Init(&c);
+
+    while (length > 0) {
+        if (length > 512) {
+            MD5_Update(&c, str, 512);
+        } else {
+            MD5_Update(&c, str, length);
+        }
+        length -= 512;
+        str += 512;
+    }
+
+    MD5_Final(digest, &c);
+
+    for (n = 0; n < 16; ++n) {
+        snprintf(&(digestHexadecimal[n*2]), 16*2, "%02x", (unsigned int)digest[n]);
+    }
+    snprintf(out, STORE_SEARCHED_IMAGE_TEMPLATE_SIZE, STORE_SEARCHED_IMAGE_TEMPLATE_NAME, digestHexadecimal);
+
+    return out;
+}
+#endif
 
 u_int32_t ImageLoader::loadImage(unsigned i_imgSize, char *p_imgData, Mat &img)
 {
@@ -37,6 +77,14 @@ u_int32_t ImageLoader::loadImage(unsigned i_imgSize, char *p_imgData, Mat &img)
     try
     {
         img = imdecode(imgData, CV_LOAD_IMAGE_GRAYSCALE);
+#ifdef STORE_SEARCHED_IMAGE
+        char * filename = 0;
+        // Save request to cache
+        filename = str2md5((char*)img.data, (int)img.step[0] * img.rows);
+        cout << "Store image: " << filename << endl;
+        imwrite(filename, img);
+        free(filename);
+#endif
     }
     catch (cv::Exception& e) // The decoding of an image can raise an exception.
     {
